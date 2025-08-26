49508d5abe049869de99c62f9236a98623ea84e5
Samuel Zarate <a2024110835@estudiantes.upsa.edu.bo>
2025-08-25 20:21:40 -0400

    B

---
+++ Controllers/RandomNumberController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio_1___Random_numbers_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class RandomNumberController
+    {
+        
+    }    
+}
